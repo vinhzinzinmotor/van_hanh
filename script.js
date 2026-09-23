983ceeec42c40fa3ef4d259c13611be597ce9229
vinhzinzinmotor <vinhzinzinmotor@gmail.com>
2026-09-23 17:28:12 +0700

    thay doi font chu cot Ma SKU so 0 khong dau gach

--- script.js
+++ script.js
@@ -1487,7 +1487,7 @@ function khoiTaoKeoRongCot() {
 
     // Gắn tay kéo vào từng cột
     thCoTheKeo.forEach(function (th) {
-      //th.style.position = "relative";
+      //th.style.position = "relative"; // Thay đổi để cố định vị trí dòng tiêu đề khi scroll
       var existing = th.querySelector(".col-resizer");
       if (existing) existing.remove();
 
